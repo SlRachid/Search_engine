089944de321024642085de75fbb1209b92ff564e
Slrachid <sot.rdsi@gmail.com>
2023-11-28 22:27:26 +0100

    Data loading function

---
+++ Data_loading.py
@@ -0,0 +1,11 @@
+import os
+import py7zr
+
+MAIN_PATH = ".\Search_engine"
+DATA_PATH = ".\Search_engine\data"
+
+os.mkdir(DATA_PATH)
+
+archive = py7zr.SevenZipFile('/content/drive/MyDrive/datascience.stackexchange.com.7z', mode='r')
+archive.extractall(path=os.path.join(MAIN_PATH, 'data'))
+archive.close()
